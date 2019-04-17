7b41bf8854749479357c94ef6c9a3620f2f09d54
Nesterov, Viacheslav <viacheslav.nesterov@metlife.com>
2019-04-17 15:38:29 -0400

    Initial commit

---
+++ rename_files.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Python code to rename multiple  
+files in a directory or folder 
+
+"""
+# importing os module 
+import os
+
+# current path to directory with files
+path = os.getcwd()+"/Rename_dir/"
+  
+# Function to rename multiple files 
+def rename_files(path, new_name):
+    ''' Renames all files in specified directory to one standard name
+        with order number at the end, i.e new_name_1, new_name_2, etc.
+        Takes:
+        path - string - as path to directory with files
+        new_name - string - as new file name for all files in directory'''
+    # counter
+    count = 1  
+    for filename in os.listdir(path):
+        # getting extention
+        ext = "."+filename.split(".")[1]
+        os.rename(path+filename, path+new_name+"_"+str(count)+ext)
+        print("Renamed from {} to {}".format(filename, new_name+"_"+str(count)+ext))
+        count += 1
+
+rename_files(path, "New_name")
